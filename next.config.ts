f69e1cf9dbd0707c015fb3fd8d66dace9c38cf89
jtackesiii <jtackesiii@gmail.com>
2025-05-28 11:19:27 -0400

    remote patterns

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,25 @@ const nextConfig: NextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  image: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.notion.so',
+        pathname: '/image/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'notion.so',
+        pathname: '/image/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3-us-west-2.amazonaws.com',
+        pathname: '/public.notion-static.com/**',
+      },
+    ],
+  },
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
     useCache: true,
